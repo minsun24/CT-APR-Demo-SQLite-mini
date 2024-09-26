a84862551c727bb4d79c76f4e738760e72b69da3
Yeongcheol Kim <aree91@naver.com>
2024-09-26 10:27:48 +0900

    Added test.c for sbfl cluster example

---
+++ src/test.c
@@ -0,0 +1,23 @@
+int testme(int a, int b) {
+	if (a < b) {
+		a = 2*a;
+	}
+	else {
+		b = 3*b;
+	}
+	return a + b;
+}
+
+int testme2(int a, int b) {
+	return testme(a, b);
+}
+
+int testme3(int a, int b) {
+	if (a < b) {
+		a = 2*a;
+	}
+	else {
+		b = 3*b;
+	}
+	return a + b;
+}
